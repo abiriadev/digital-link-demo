23a697f856a034591413d7710c4264090e06e8c5
Abiria <abiria.dev@gmail.com>
2023-09-06 00:03:47 +0900

    feat: add DLRS wrapper

--- resolver/src/app.service.ts
+++ resolver/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common'
+import { DigitalLinkWasm } from 'digital-link-rs'
 
 @Injectable()
 export class AppService {
 	resolveDigitalLink(url: string): string {
 		return url
+	}
+
+	parseDigitalLink(
+		rawUrl: string,
+	): DigitalLinkWasm | undefined {
+		return DigitalLinkWasm.try_from_str(rawUrl)
 	}
 }
