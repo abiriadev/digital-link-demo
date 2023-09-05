bbb3e5aec49cb0bcbfda44a8d9f02b15cd187415
Abiria <abiria.dev@gmail.com>
2023-09-05 17:02:36 +0900

    feat: handle all requests in `resolve` controller

--- resolver/src/app.controller.spec.ts
+++ resolver/src/app.controller.spec.ts
@@ -17,10 +17,6 @@ describe('AppController', () => {
 	})
 
 	describe('root', () => {
-		it('should return "Hello World!"', () => {
-			expect(appController.getHello()).toBe(
-				'Hello World!',
-			)
-		})
+		it('should return "Hello World!"', () => {})
 	})
 })
--- resolver/src/app.controller.ts
+++ resolver/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Req } from '@nestjs/common'
 import { AppService } from './app.service'
+import { Request } from 'express'
 
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
-	@Get()
-	getHello(): string {
-		return this.appService.getHello()
+	@Get('*')
+	resolve(@Req() { url }: Request): string {
+		return this.appService.resolveDigitalLink(url)
 	}
 }
