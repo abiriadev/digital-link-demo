f3975551a851281f7acc6ffa3a787eb090cf0d66
Abiria <abiria.dev@gmail.com>
2023-09-05 17:38:05 +0900

    feat: accept `linkType`

--- resolver/src/app.controller.ts
+++ resolver/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req } from '@nestjs/common'
+import { Controller, Get, Query, Req } from '@nestjs/common'
 import { AppService } from './app.service'
 import { Request } from 'express'
 
@@ -7,7 +7,10 @@ export class AppController {
 	constructor(private readonly appService: AppService) {}
 
 	@Get('*')
-	resolve(@Req() { url }: Request): string {
+	resolve(
+		@Req() { url }: Request,
+		@Query('linkType') linkType: string,
+	): string {
 		return this.appService.resolveDigitalLink(url)
 	}
 }
