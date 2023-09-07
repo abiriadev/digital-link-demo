6c65e79bbe87531909e6e90c8686a972c0ca94dc
Abiria <abiria.dev@gmail.com>
2023-09-07 11:54:51 +0900

    feat: add logging feature with `trace`

--- resource-provider/src/main.rs
+++ resource-provider/src/main.rs
@@ -4,9 +4,12 @@ use axum::{
 	routing::get,
 	Router, Server,
 };
+use tracing_subscriber::fmt::init;
 
 #[tokio::main]
 async fn main() {
+	init();
+
 	let app = Router::new().route("/:pid", get(handle_product));
 
 	Server::bind(&"0.0.0.0:3535".parse().unwrap())
