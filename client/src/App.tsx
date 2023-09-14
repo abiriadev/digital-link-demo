e20172c10e974a82711b8cf3c8cc43d71f6b5f89
Abiria <abiria.dev@gmail.com>
2023-09-15 07:34:27 +0900

    feat: implement image pasting feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,74 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { invoke } from '@tauri-apps/api/tauri'
 import './App.css'
 
 function App() {
-	const [greetMsg, setGreetMsg] = useState('')
-	const [name, setName] = useState('')
-
-	async function greet() {
-		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-		setGreetMsg(await invoke('greet', { name }))
-	}
+	const [blob, setBlob] = useState<Blob | null>(null)
 
 	return (
-		<div className="container">
+		<div
+			className="container"
+			onPaste={async () => {
+				try {
+					const a = (
+						await navigator.clipboard.read()
+					)[0]
+
+					const mime = a.types.find(type =>
+						type.startsWith('image/'),
+					)
+
+					if (!mime) return
+
+					const blob1 = await a.getType(mime)
+
+					setBlob(blob1)
+				} catch {}
+			}}
+		>
 			<h1>Welcome to Tauri!</h1>
 
-			<div className="row">
-				<a
-					href="https://vitejs.dev"
-					target="_blank"
-				>
-					<img
-						src="/vite.svg"
-						className="logo vite"
-						alt="Vite logo"
-					/>
-				</a>
-				<a href="https://tauri.app" target="_blank">
-					<img
-						src="/tauri.svg"
-						className="logo tauri"
-						alt="Tauri logo"
-					/>
-				</a>
-				<a
-					href="https://reactjs.org"
-					target="_blank"
-				>
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-
-			<p>
-				Click on the Tauri, Vite, and React logos to
-				learn more.
-			</p>
-
-			<form
-				className="row"
-				onSubmit={e => {
-					e.preventDefault()
-					greet()
-				}}
-			>
-				<input
-					id="greet-input"
-					onChange={e =>
-						setName(e.currentTarget.value)
-					}
-					placeholder="Enter a name..."
-				/>
-				<button type="submit">Greet</button>
-			</form>
-
-			<p>{greetMsg}</p>
+			<img
+				src={
+					blob
+						? window.URL.createObjectURL(blob)
+						: undefined
+				}
+			></img>
 		</div>
 	)
 }
