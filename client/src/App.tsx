e8fe923e981ebd5a5da8bb768da8f6352e649388
Abiria <abiria.dev@gmail.com>
2023-09-15 08:06:23 +0900

    feat: add QR detection feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
+import { Html5Qrcode } from 'html5-qrcode'
 
 function App() {
 	const [blob, setBlob] = useState<Blob | null>(null)
+
+	const qr = useRef<Html5Qrcode | null>(null)
+
+	useEffect(() => {
+		qr.current = new Html5Qrcode('qr')
+	}, [])
 
 	return (
 		<div
@@ -22,6 +29,13 @@ function App() {
 					const blob1 = await a.getType(mime)
 
 					setBlob(blob1)
+
+					const res = await qr.current?.scanFile(
+						new File([blob1], 'qr'),
+						false,
+					)
+
+					console.log(res)
 				} catch {}
 			}}
 		>
@@ -33,7 +47,13 @@ function App() {
 						? window.URL.createObjectURL(blob)
 						: undefined
 				}
+				width={300}
+				style={{
+					margin: '0 auto',
+				}}
 			></img>
+
+			<div id="qr"></div>
 		</div>
 	)
 }
