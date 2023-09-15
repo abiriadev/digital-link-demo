25fd5bd521796e29d95861d32a1215c18a3898f6
Abiria <abiria.dev@gmail.com>
2023-09-15 13:46:16 +0900

    feat: implement GTIN rendering

--- catalog/src/App.tsx
+++ catalog/src/App.tsx
@@ -1,20 +1,34 @@
 import './App.css'
 import data from '../../data-collector/products.json'
 import QRCode from 'react-qr-code'
+import { useState } from 'react'
+
+const lh = 'http://localhost'
 
 function App() {
+	const [baseUrl, setBaseUrl] = useState(lh)
+
 	return (
 		<>
 			<h1>GS1 Digital Link Demo Products QRcode</h1>
+			<input
+				value={baseUrl}
+				onChange={e => setBaseUrl(e.target.value)}
+			/>
+			<button onClick={() => setBaseUrl(lh)}>
+				Reset
+			</button>
 			<ul>
-				{data.map(({ url, name, image }) => (
+				{data.map(({ gtin, name, image }) => (
 					<li>
 						<h2>{name}</h2>
 						<img
 							src={image}
 							width="300px"
 						></img>
-						<QRCode value={url} />
+						<QRCode
+							value={`${baseUrl}/01/${gtin}`}
+						/>
 					</li>
 				))}
 			</ul>
