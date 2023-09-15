e716f38570840e58866595aab377bbbe34a1b8d9
Abiria <abiria.dev@gmail.com>
2023-09-15 10:56:12 +0900

    feat: add CLI part

---
+++ check-digit-calc/src/main.rs
@@ -0,0 +1,31 @@
+use check_digit_calc::Gtin;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Arg {
+	#[command(subcommand)]
+	command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+	Validate { gtin: String },
+	Calc { partial_gtin: String },
+}
+
+fn main() {
+	let arg = Arg::parse();
+
+	match arg.command {
+		Commands::Validate { gtin } => match Gtin::try_from(gtin.as_str()) {
+			Ok(_) => println!("OK"),
+			Err(_) => println!("validation failed."),
+		},
+		Commands::Calc { partial_gtin } =>
+			match Gtin::calc_check_digit_from_str(&partial_gtin) {
+				Ok(d) => println!("check digit: {d}"),
+				Err(_) => println!("validation failed."),
+			},
+	}
+}
