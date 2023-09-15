4c76f3426be2a4b8dafc3b1bf6dd6c8a9845b6ba
Abiria <abiria.dev@gmail.com>
2023-09-15 12:46:43 +0900

    feat: add Product to ModelEntry serialization process

--- data-collector/src/models/product.rs
+++ data-collector/src/models/product.rs
@@ -1,3 +1,7 @@
+use std::iter::once;
+
+use serde::Serialize;
+
 use super::{
 	catalog::CatalogParserProduct, Manual, ManualRequest, ResolveRequest,
 };
@@ -43,6 +47,105 @@ impl Product {
 			url: format!("{base_url}/sec/{goods_detail_url}"),
 			spec: format!("spec: {goods_nm}"),
 			manuals: manuals.unwrap_or(vec![]),
+		}
+	}
+}
+
+#[derive(Serialize)]
+#[serde(rename_all(serialize = "camelCase"))]
+struct ResolverEntry {
+	identification_key_type: String, // "gtin",
+	identification_key: String,      // "00745883713370",
+	item_description: String,        // "M00420020590",
+	qualifier_path: String,          // "/",
+	active: bool,
+	responses: Vec<ResolverEntryResponse>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all(serialize = "camelCase"))]
+struct ResolverEntryResponse {
+	link_type: String,           // "gs1:pip",
+	iana_language: String,       // "en",
+	context: String,             // "us",
+	mime_type: String,           // "text/html",
+	link_title: String,          // "Product Info",
+	target_url: String,          // "http://lansley.com/test.html",
+	default_link_type: bool,     // true,
+	default_iana_language: bool, // true,
+	default_context: bool,       // true,
+	default_mime_type: bool,     // true,
+	fwqs: bool,                  // false,
+	active: bool,                // true
+}
+
+impl Default for ResolverEntryResponse {
+	fn default() -> Self {
+		Self {
+			link_type: "gs1:pip".to_owned(),   // req
+			iana_language: "ko".to_owned(),    // ?
+			context: "kr".to_owned(),          // ?
+			mime_type: "text/html".to_owned(), // req
+			link_title: Default::default(),    // req
+			target_url: Default::default(),    // req
+			default_link_type: false,          // req
+			default_iana_language: true,
+			default_context: true,
+			default_mime_type: true,
+			fwqs: true,
+			active: true,
+		}
+	}
+}
+
+impl From<Product> for ResolverEntry {
+	fn from(
+		Product {
+			model_code,
+			name,
+			image,
+			url,
+			spec,
+			manuals,
+		}: Product,
+	) -> Self {
+		Self {
+			identification_key_type: "gtin".to_owned(), // fix
+			identification_key: "12345678901231".to_owned(), // should be generated later
+			item_description: name.clone(),
+			qualifier_path: "/".to_owned(), // fix
+			active: true,                   // fix
+			responses: manuals
+				.into_iter()
+				.map(|m| m.into())
+				.chain(once(ResolverEntryResponse::from_url(
+					url, name,
+				)))
+				.collect::<Vec<_>>(), // .concat(),
+		}
+	}
+}
+
+impl From<Manual> for ResolverEntryResponse {
+	fn from(Manual { name, href }: Manual) -> Self {
+		Self {
+			link_type: "gs1:instructions".to_owned(), // all manuals are instructions
+			mime_type: "application/pdf".to_owned(),  // all manuals are PDF documents
+			link_title: name,
+			target_url: href,
+			..Default::default()
+		}
+	}
+}
+
+impl ResolverEntryResponse {
+	fn from_url(url: String, name: String) -> Self {
+		Self {
+			link_type: "gs1:pip".to_owned(), // product page is pip
+			link_title: name,
+			target_url: url,
+			default_link_type: true,
+			..Default::default()
 		}
 	}
 }
